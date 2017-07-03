12f2530e9ca0f9bba39eedc3de9ddfbf4c2aeab2
Nischal Samji <nischalsamji@gmail.com>
2017-07-02 23:42:40 -0400

    Exercise 1 Complete

--- code/src/com/allendowney/thinkdast/ListClientExample.java
+++ code/src/com/allendowney/thinkdast/ListClientExample.java
@@ -1,25 +1,24 @@
 package com.allendowney.thinkdast;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class ListClientExample {
-	@SuppressWarnings("rawtypes")
 	private List list;
 
-	@SuppressWarnings("rawtypes")
+
 	public ListClientExample() {
-		list = new LinkedList();
+		list = new ArrayList();
 	}
 
-	@SuppressWarnings("rawtypes")
+
 	public List getList() {
 		return list;
 	}
 
 	public static void main(String[] args) {
 		ListClientExample lce = new ListClientExample();
-		@SuppressWarnings("rawtypes")
 		List list = lce.getList();
 		System.out.println(list);
 	}
